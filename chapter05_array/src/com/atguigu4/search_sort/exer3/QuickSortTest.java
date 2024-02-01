0cd9e380c72df33646ba984212a9824453983eec
heartuser <546239212@qq.com>
2024-02-01 20:07:49 +0800

    123

--- chapter05_array/src/com/atguigu4/search_sort/exer3/QuickSortTest.java
+++ chapter05_array/src/com/atguigu4/search_sort/exer3/QuickSortTest.java
@@ -13,16 +13,13 @@ public class QuickSortTest {
 
     private static void quickSort(int[] date, int start, int end) {
         // start、end遍历范围的两端
-        if (start > end) {
-            return;
-        } else {
+        if (start < end) {
             int base = date[start];
             int low = start;
-            int high = end;
+            int high = end + 1;
             while (true) {
-                // 从前到后遍历
-                while (low < end && date[++low] > base) ;
-                while (high > start && date[--high] < base) ;
+                while (low < end && date[++low] <= base) ;
+                while (high > start && date[--high] >= base) ;
                 if (low < high)
                     swap(date, low, high);
                 else
@@ -31,16 +28,17 @@ public class QuickSortTest {
             // 交换start和high之间的数字
             swap(date, start, high);
             // 再处理前后两段
-            quickSort(date, 0, high - 1);
+            quickSort(date, start, high - 1);
             quickSort(date, high + 1, end);
         }
     }
 
-    private static void swap(int[] arr, int index1, int index2) {
-        arr[index1] = arr[index1] ^ arr[index2];
-        arr[index2] = arr[index1] ^ arr[index2];
-        arr[index1] = arr[index1] ^ arr[index2];
+    private static void swap(int[] date, int index1, int index2) {
+        int temp = date[index1];
+        date[index1] = date[index2];
+        date[index2] = temp;
+//        date[index1] = date[index1] ^ date[index2];
+//        date[index2] = date[index1] ^ date[index2];
+//        date[index1] = date[index1] ^ date[index2];
     }
-
-    ;
 }
