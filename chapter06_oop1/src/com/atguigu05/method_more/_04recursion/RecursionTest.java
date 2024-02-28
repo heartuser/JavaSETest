7a365a6ee138553873b78879cb1600b4f6bd760d
heartuser <546239212@qq.com>
2024-02-28 19:20:29 +0800

    方法练习

--- chapter06_oop1/src/com/atguigu05/method_more/_04recursion/RecursionTest.java
+++ chapter06_oop1/src/com/atguigu05/method_more/_04recursion/RecursionTest.java
@@ -5,6 +5,34 @@ package com.atguigu05.method_more._04recursion;
  * @create 16:57
  */
 public class RecursionTest {
+    public static void main(String[] args) {
+        RecursionTest r = new RecursionTest();
+        System.out.println(r.getSum(100));
+        System.out.println(r.getMul(5));
+        System.out.println(r.f(10));
+    }
 
+    public int getSum(int num) {// 递归计算1到100的和
+        if (num == 1) {
+            return 1;
+        } else {
+            return getSum(num - 1) + num;
+        }
+    }
 
+    public int getMul(int num) {// 计算n!
+        if (num == 1) {
+            return 1;
+        } else {
+            return getMul(num - 1) * num;
+        }
+    }
+
+    public int f(int n) {// 斐波那契数列
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return f(n - 1) + f(n - 2);
+        }
+    }
 }
